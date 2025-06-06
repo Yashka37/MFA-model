28e25d5f0404fb7af8a444262d2fc647e152aa6a
Yashka37 <105378410+Yashka37@users.noreply.github.com>
2025-06-06 09:30:13 +0300

    UPD

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
 <?php
+header("Content-Security-Policy: default-src 'self';
+        img-src 'self'; script-src 'self'; object-src 'none';
+        base-uri 'none'; frame-ancestors 'none';");
 session_start();
 //error_reporting(E_ALL); //ini_set('display_errors', 1);
 
